6144bf58709b85eb77e6e6acede036fb030719ee
Sakthi Ganesh <sakthiganesh1997@gmail.com>
2021-01-26 22:30:10 +0530

    removed py scripts

--- loading_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-
-imdb_df = pd.read_csv('IMDB_Dataset.csv', index_col = None)
-imdb_df = imdb_df.head(10000)
--- loading_data_2.py
+++
@@ -1,8 +0,0 @@
-label = []
-
-for index, row in imdb_df.iterrows():
-    if(row['sentiment'] == 'positive'): label.append(1)
-    else: label.append(0)
-
-imdb_df['label'] = label
-imdb_df = imdb_df.drop(['sentiment'], axis = 1)
